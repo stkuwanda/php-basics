96a3a7c9c7e2432f567eb8339f60ef85a273266f
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-08-27 13:21:19 +0200

    outputting html string nodes

---
+++ hello_world/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    echo "Hello, World! <br>";
+    echo ' This is a simple PHP web page. <br>';
+    echo 'Concatenation: ' . 'Hello' . ', ' . 'World' . '!' . '<br>';
+  ?>
+</body>
+</html>
