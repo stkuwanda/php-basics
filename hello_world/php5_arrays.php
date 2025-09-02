f825a4ee04a51e20fa059e7c5cbb8ee1bab2d2fd
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-02 10:05:00 +0200

    adding elementes to array

--- hello_world/php5_arrays.php
+++ hello_world/php5_arrays.php
@@ -1,8 +1,19 @@
 <?php 
 // Indexed array
 $colors = array("Red", "Green", "Blue");
+
+// Adding elements to the array
+// by defining the index key.
+// note you can set any index key you want.
+// order does not matter.
+$colors[4] = "Yellow";
+$colors[3] = "Black";
+
 echo $colors[0] . ' ';
 echo $colors[1] . ' ';
-echo $colors[2];
+echo $colors[2] . ' ';
+echo $colors[3] . ' ';
+echo $colors[4] . ' ';
 echo "<br>";
+var_dump($colors);
 
