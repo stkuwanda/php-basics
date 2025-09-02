dcf4ca824b34999b74aca45a0fbfdaf5b9bb2c6a
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-02 10:50:11 +0200

    useful functions on arrays

--- hello_world/php5_arrays.php
+++ hello_world/php5_arrays.php
@@ -59,3 +59,69 @@ echo "<pre>";
 var_dump($user);
 echo "</pre>";
 
+// array_pop
+// removes the last element from an array
+echo "<pre>";
+array_pop($user['hobbies']);
+var_dump($user);
+echo "</pre>";
+
+// array_push
+// adds one or more elements to the end of an array
+array_push($user['hobbies'], "traveling", "swimming");
+echo "<pre>";
+var_dump($user);
+echo "</pre>";
+
+// count
+// counts all elements in an array or something in an object
+echo "<pre>";
+echo "Number of hobbies: " . count($user['hobbies']) . "\n";
+echo "Number of properties in user: " . count($user) . "\n";
+echo "</pre>";
+
+// sort
+// sorts an array in ascending order
+$numbers = [5, 2, 9, 1, 5, 6];
+sort($numbers);
+echo "<pre>";
+var_dump($numbers);
+echo "</pre>";
+
+// rsort
+// sorts an array in descending order
+rsort($numbers);
+echo "<pre>";
+var_dump($numbers);
+echo "</pre>";
+
+// asort
+// sorts an associative array in ascending order, according to the values
+asort($person);
+echo "<pre>";
+var_dump($person);
+echo "</pre>";
+
+// ksort
+// sorts an associative array in ascending order, according to the keys
+ksort($person);
+echo "<pre>";
+var_dump($person);
+echo "</pre>";
+
+// krsort
+// sorts an associative array in descending order, according to the keys
+krsort($person);
+echo "<pre>";
+var_dump($person);
+echo "</pre>";
+
+// in_array
+// checks if a value exists in an array
+echo "<pre>";   
+if (in_array("John", $person)) {
+    echo "John is in the person array\n";
+} else if (in_array("", $person)) {
+    echo "Empty string is in the person array\n";
+}
+
