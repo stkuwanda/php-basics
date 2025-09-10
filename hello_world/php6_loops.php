89f67c04ecc51de6ddac6a669cde8a86dfb6be5e
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-10 19:42:15 +0200

    commentary on the use cases of each loop type

--- hello_world/php6_loops.php
+++ hello_world/php6_loops.php
@@ -7,11 +7,13 @@
 // 4. foreach loop
 
 // for loop example
+// useful if you know in advance how many times you want to iterate
 for ($i = 1; $i <= 5; $i++) {
   echo "Iteration: $i<br>";
 }
 
 // while loop example
+// useful if you want to loop until a certain condition is met
 $j = 1;
 while ($j <= 5) {
   echo "Count: $j<br>";
@@ -19,6 +21,7 @@ while ($j <= 5) {
 }
 
 // do-while loop example
+// similar to while loop, but guarantees at least one execution
 $k = 1;
 do {
   echo "Number: $k<br>";
@@ -26,6 +29,7 @@ do {
 } while ($k <= 5);
 
 // foreach loop example
+// useful for iterating over arrays
 $colors = array("Red", "Green", "Blue", "Yellow", "Purple");
 foreach ($colors as $color) {
   echo "Color: $color<br>";
