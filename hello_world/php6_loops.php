4a67e4bd81d04443ddf8773897b01b63936cd144
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-10 19:32:16 +0200

    for loop

---
+++ hello_world/php6_loops.php
@@ -0,0 +1,12 @@
+<?php
+// Loops in PHP
+// There are several types of loops in PHP:
+// 1. while loop
+// 2. do-while loop
+// 3. for loop
+// 4. foreach loop
+
+// for loop example
+for ($i = 1; $i <= 5; $i++) {
+  echo "Iteration: $i<br>";
+}
