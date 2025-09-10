2badda11b3436daa3cd955b5207ae42ac86bc39a
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-10 16:23:43 +0200

    conditional display

--- hello_world/index.php
+++ hello_world/index.php
@@ -1,3 +1,6 @@
+<?php
+$is_student = true;
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,6 +20,11 @@
   <h1>Hello, <?= " World!" ?></h1>
   <h2>Hi, <?php echo " Simba!" ?></h2>
   <p>Today's date is <?= date('m-d-Y') ?></p>
+  <?php if ($is_student) {
+    echo "<p>You are a student.</p>";
+  } else {
+    echo "<p>You are not a student.</p>";
+  } ?>
 </body>
 
 </html>
